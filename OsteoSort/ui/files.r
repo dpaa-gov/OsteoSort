65d7f74e080f97832c96d2eb6e7c225eb6b1242b
Jeff Lynch <jjlynch@osteocoder.com>
2026-02-10 15:25:20 -0600

    template button fix

--- OsteoSort/ui/files.r
+++ OsteoSort/ui/files.r
@@ -1,4 +1,5 @@
-files <- navbarMenu("Files",icon = icon("folder-open", lib="font-awesome"),
-    HTML("<a id='template' class='btn btn-default shiny-download-link disabled' href='' target='_blank' download aria-disabled='true' tabindex='-1'>Template</a>"),
-    HTML("<a id='example' class='btn btn-default shiny-download-link disabled' href='' target='_blank' download aria-disabled='true' tabindex='-1'>Example</a>")
+files <- navbarMenu("Files",
+    icon = icon("folder-open", lib = "font-awesome"),
+    HTML("<a id='template' class='btn shiny-download-link disabled' href='' target='_blank' download aria-disabled='true' tabindex='-1' style='background-color:#d4a843; border-color:#d4a843; color:#fff;'>Template</a>"),
+    HTML("<a id='example' class='btn shiny-download-link disabled' href='' target='_blank' download aria-disabled='true' tabindex='-1' style='background-color:#d4a843; border-color:#d4a843; color:#fff;'>Example</a>")
 )
